f80a321d36bc7bf10f0c95b2c8e6cc8a742a6711
arnabisen023 <senarnabi915@gmail.com>
2024-04-25 16:53:05 +0530

    24/04/2024 hero section complete 1st commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontsize:{
+        as:"0.75rem",
+        jk:"0.875rem",
+        cs:"1rem",
+        bb:"1.25rem",
+        ss:"1.5rem",
+        bn:"1.75rem",
+        sd:"2.25rem",
+      }
+    },
   },
   plugins: [],
 }
