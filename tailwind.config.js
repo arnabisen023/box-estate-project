4755a8236d57354219dd542ad20e7110bb2b9af4
arnabisen023 <senarnabi915@gmail.com>
2024-04-30 20:46:46 +0530

    30/04/2024 Figma design complete.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,16 @@ export default {
         base:"0.875rem",
         md:"1rem",
         lg:"1.25rem",
-        "xl":"1.5rem",
+        xl:"1.5rem",
         "2xl":"1.75rem",
         "3xl":"2.25rem",
+        "4xl":"3rem",
+        "5xl":"4rem"
       },
 
       backgroundImage: {
-        'team-bg-image': "url('/public/teams/team.1')",
-        'team-avatar':"url('/public/teams/team.2')"
+        'team-bg-image': "url('/public/teams/team.1.jpeg')",
+        'team-avatar':"url('/public/teams/team.2.jpeg')"
       },
     },
   },
